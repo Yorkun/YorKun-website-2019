730478a432e53d6f9d75dc5cadb79b452c2dc517
Yorkun <726667671@qq.com>
2020-05-27 11:29:21 +0800

    Update globalFooter.vue
    add links

--- src/components/globalFooter.vue
+++ src/components/globalFooter.vue
@@ -4,6 +4,7 @@
       <span>Links:</span>
       <a href="https://yancymin.design" target="_blank">Yancy Min</a>
       <a href="https://www.liuchangyi.com/" target="_blank">Charlie Liu</a>
+      <a href="https://figma.cool/" target="_blank">FigmaCN Plugin</a>
       <a href="https://www.dribbbbble.com" target="_blank">Dark Mode for Dribbble</a>
       <!-- <a href="http://yorkunpic.lofter.com" target="_blank">
         {{footerLinkText}}
@@ -12,7 +13,7 @@
     </div>
     <div
       class="globalFooter_copyright"
-    >Designed by YorKun and coded by Yancy Min. Site is hosted on Github and deployed by ZEIT.</div>
+    >Designed by YorKun and coded by Yancy Min. Site is hosted on Github and deployed by Vercel.</div>
   </div>
 </template>
 
