61564548f62128d5c4f4d71d302059e2ff09acfb
Yorkun <726667671@qq.com>
2020-08-07 15:39:50 +0800

    -

--- src/data/cn.js
+++ src/data/cn.js
@@ -9,7 +9,6 @@ import link from '../assets/index/button-icon/link_icon.svg';
 import geetestweb from '../assets/index/geetestweb.png';
 import ui_captcha from '../assets/index/ui_captcha.gif';
 import app_onelogin from '../assets/index/app_onelogin.png';
-import Portrait from '../assets/index/IMG_3045.png';
 import gmake_logo from '../assets/index/gmake_logo2.png';
 import banana from '../assets/index/banana_banner.gif';
 import myDribbble from '../assets/index/mydribbble.png';
