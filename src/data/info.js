e62fff0cd7f8efc7dad50fe59d8d6880a3f1347b
Yorkun Cheng <yorkuncheng@gmail.com>
2024-03-14 15:00:06 +0800

    Update small_logo.svg

--- src/data/info.js
+++ src/data/info.js
@@ -3,8 +3,8 @@ export default {
     about: [
       'About me',
       `Hey peeps, my name is YorKun. A UI designer and an animation enthusiast. You could simply call me "york".<br/><br/>
-       I started to get obsessed with drawing when I was 12, I remember spending a whole weekend in the drawing class at that time. Naturally, I ended up with studying art in high school. From there, I went to Hubei Academy of Fine Arts to continue pursuing my art career.<br/><br/>
-       I love solving problems with my design mindset and dare to face more challenges, with humility and hard work. Serenity to accept the things I can't change, struggle to change the things I can.`,
+      At 12, I became obsessed with drawing, spending entire weekends in drawing classes. This led me to study art in high school and pursue my art career at Hubei Academy of Fine Arts. <br/><br/>
+      I enjoy problem-solving with a design mindset, fearlessly facing challenges with humility and hard work. Embracing serenity for the things I can't change, and striving to change what I can. `,
     ],
     contact: {
       title: 'Contact me',
@@ -13,8 +13,8 @@ export default {
   cn: {
     about: [
       '关于我',
-      `你好，我叫 YorKun，是一名设计师，从事视觉、界面、动效等设计工作。你可以简单称呼我「右可」。<br/><br/>
-       因为对画画感兴趣，我小学时周末通常是在绘画兴趣班度过的，随后高中开始系统的学习美术，大学于湖北美术学院动画系就读。可能是在学习环境中接触「实验性」和「纯艺」比较多，我更向往贴近生活的视觉表现形式，希望利用所学知识来解决实际问题。<br/><br/>
+      `你好，我叫右可，是一名设计师，从事视觉、界面、动效等设计工作。<br/><br/>
+       因为对画画感兴趣，我小学时周末通常是在绘画兴趣班度过的，随后高中开始系统的学习美术，大学于湖北美术学院动画系就读。可能是在学习环境中接触「实验性」和「纯艺」比较多，我更向往贴近生活的表现形式，希望利用所学知识来解决实际问题。<br/><br/>
        我希望自己能一直热爱设计并敢于面对更多挑战。努力改变能改变的，平静接受不能改变的。`,
     ],
     contact: {
